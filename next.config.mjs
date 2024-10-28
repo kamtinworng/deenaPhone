14be846b5642179b4a3d5a2bab0a8a728d5a878e
mkamtinworng <mkamtinworng@gmail.com>
2024-10-28 19:26:51 +0700

    0.2.1 next-config

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: "dist",
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/api/auth/Login",
-        permanent: false,
-      },
-    ];
-  },
+  // async redirects() {
+  //   return [
+  //     {
+  //       source: "/",
+  //       destination: "/api/auth/Login",
+  //       permanent: false,
+  //     },
+  //   ];
+  // },
 };
 
 export default nextConfig;
