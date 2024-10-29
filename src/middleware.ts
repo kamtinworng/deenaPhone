2d46cb42a935aed027af9231d31e2fead3ccdf3f
mkamtinworng <mkamtinworng@gmail.com>
2024-10-29 11:24:20 +0700

    0.2.5 middleware update reponse to Home

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,10 +2,10 @@ import { NextResponse } from "next/server";
 
 export function middleware(request: NextResponse) {
   // const cookie = request.cookies.get("next-auth.session-token");
-  const cookie = request.cookies.get("_Secure-next-auth.session-token");
+  const cookie = request.cookies.get("__Secure-next-auth.session-token");
 
   if (cookie) return NextResponse.next();
-  return NextResponse.redirect(new URL("/", request.url));
+  return NextResponse.redirect(new URL("/Home", request.url));
 }
 
 export const config = {
